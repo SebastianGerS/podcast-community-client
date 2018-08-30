671b9e884e5cd69e6863357cee0f48fece355d53
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-08-30 23:25:36 +0200

    create searchbar component

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBar = () => (
+  <div className="searchbar">
+    <input placeholder="Search..." />
+    <div>
+      <button type="button">Register</button>
+    </div>
+  </div>
+);
+
+export default SearchBar;
