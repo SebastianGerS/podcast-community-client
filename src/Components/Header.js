221832b18ab7b0eefd58f47b2621e580a381d822
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-08-30 16:46:49 +0200

    add basic styling for header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,16 @@ import logo from '../Assets/Images/logo.png';
 
 const Header = () => (
   <header>
-    <img src={logo} className="logo" alt="logo" />
-    <h1 className="site-title">Thrue the Ether</h1>
+    <figure>
+      <img src={logo} className="logo" alt="logo" />
+    </figure>
+    <div>
+      <h1 className="site-title">Thrue the Ether</h1>
+      <p> – a podcast community</p>
+    </div>
+    <div>
+      <button type="button">Loggin</button>
+    </div>
   </header>
 );
 
