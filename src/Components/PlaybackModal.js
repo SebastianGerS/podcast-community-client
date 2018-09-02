8b9d524cbdd8006c6c791ce7128a4fee41818c3d
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-02 22:33:41 +0200

    create playbackmodal with styles

---
+++ src/Components/PlaybackModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PlaybackModal = ({
+  episode, isPlaying, togglePlay,
+}) => (
+  <div className="playbackmodal">
+    <figure>
+      <img src={episode.thumbImg} alt="podcast-img" className="podcast-img" />
+    </figure>
+    <div>
+      <line />
+    </div>
+    <p>{episode.title}</p>
+    <div className="playback-controles">
+      <button type="button" className="backward" />
+      <button type="button" className={isPlaying ? 'pause' : 'play'} onClick={togglePlay} />
+      <button type="button" className="forward" />
+    </div>
+  </div>
+);
+
+PlaybackModal.propTypes = {
+  episode: PropTypes.objectOf(PropTypes.string).isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  togglePlay: PropTypes.func.isRequired,
+};
+
+export default PlaybackModal;
