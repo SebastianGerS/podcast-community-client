5658ad1eacfeffa29855e0207a610fcc21cddca4
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-04 02:32:59 +0200

    Add logic to handle drag for progressbar
    	* add onDragEnd function to check users mouse position when draging and sedning new position to playbackcenter
    	* add props and prop validation to get currentposition and episodeLength from playbackcenter (passed via playbackmodal)
    	* use format time to get right format on currentposition and episodeLength for display

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,40 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getStartingPoint, formatTime, getSecondsPerPixel } from '../Helpers/Helpers';
 
 class ProgressBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      currentPosition: 0,
-      episodeLength: 4800,
-    };
+    this.handleDrag = this.handleDrag.bind(this);
+  }
+
+  handleDrag(e) {
+    const { episode, updatePosition } = this.props;
+    const { episodeLength } = episode;
+
+    const newPosition = ((e.clientX - getStartingPoint()) * getSecondsPerPixel(episodeLength));
+
+    updatePosition(newPosition);
   }
 
   render() {
-    const { currentPosition, episodeLength } = this.state;
+    const { episode } = this.props;
+    const { currentPosition, episodeLength } = episode;
+    const precent = (currentPosition / episodeLength) * 100;
+
     const style = {
-      width: `${(currentPosition / episodeLength) * 100}%`,
+      width: `${precent > 100 ? 100 : precent}%`,
     };
 
 
     return (
       <div className="progress-bar">
         <div className="info">
-          <p>14:50</p>
-          <p>-1:06:41</p>
+          <p>{formatTime(currentPosition)}</p>
+          <p>{`- ${formatTime(episodeLength - currentPosition)}`}</p>
         </div>
         <div className="bar">
           <div className="start" />
           <div className="line">
-            <div className="progress" style={style}>
-              <div className="marker" />
+            <div className="progress" style={style} onDragEnd={e => this.handleDrag(e)}>
+              <div draggable className="marker" onDrag={this.handleDrag} />
             </div>
           </div>
-          <div className="end" />
+          <div className="end" style={precent >= 100 ? { backgroundColor: '#FEF6F6' } : {}} />
         </div>
       </div>
     );
   }
 }
-
+ProgressBar.propTypes = {
+  episode: PropTypes.shape({
+    thumbImg: PropTypes.string,
+    title: PropTypes.string,
+    currentPosition: PropTypes.number,
+    episodeLength: PropTypes.number,
+  }).isRequired,
+  updatePosition: PropTypes.func.isRequired,
+};
 export default ProgressBar;
