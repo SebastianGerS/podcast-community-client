14782cf3bb02a99af6f0090247a29a0d9dcd9c6c
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-04 02:39:48 +0200

    Add listeners to forward and backward buttons
    	* use functions passed as props from playbackcenter with onClick listeners for backward and forward buttons
    	* pass updatePosition and epsiode to progressbar component
    	* update propTypes validation

--- src/Components/PlaybackModal.js
+++ src/Components/PlaybackModal.js
@@ -3,26 +3,34 @@ import PropTypes from 'prop-types';
 import ProgressBar from './ProgressBar';
 
 const PlaybackModal = ({
-  episode, isPlaying, togglePlay,
+  episode, isPlaying, togglePlay, updatePosition, backward, forward,
 }) => (
   <div className="playbackmodal">
     <figure>
       <img src={episode.thumbImg} alt="podcast-img" className="podcast-img" />
     </figure>
-    <ProgressBar />
+    <ProgressBar episode={episode} updatePosition={updatePosition} />
     <p>{episode.title}</p>
     <div className="playback-controles">
-      <button type="button" className="backward" />
+      <button type="button" className="backward" onClick={backward} />
       <button type="button" className={isPlaying ? 'pause' : 'play'} onClick={togglePlay} />
-      <button type="button" className="forward" />
+      <button type="button" className="forward" onClick={forward} />
     </div>
   </div>
 );
 
 PlaybackModal.propTypes = {
-  episode: PropTypes.objectOf(PropTypes.string).isRequired,
+  episode: PropTypes.shape({
+    thumbImg: PropTypes.string,
+    title: PropTypes.string,
+    currentPosition: PropTypes.number,
+    episodeLength: PropTypes.number,
+  }).isRequired,
   isPlaying: PropTypes.bool.isRequired,
   togglePlay: PropTypes.func.isRequired,
+  updatePosition: PropTypes.func.isRequired,
+  forward: PropTypes.func.isRequired,
+  backward: PropTypes.func.isRequired,
 };
 
 export default PlaybackModal;
