6d8785d1cfaa5a1e0084c6bcfc18cfe3ff3db273
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-04 02:42:02 +0200

    Add logic to handel updates of state relatet to progressbar
    	* add currentPosition and episodeLength to episode object in state
    	* add functions to update currentPosition when skipping forward or backward and when draging progressbar marker
    	* pass aditionalprops to child components

--- src/Components/PlaybackCenter.js
+++ src/Components/PlaybackCenter.js
@@ -10,6 +10,8 @@ class PlaybackCenter extends Component {
       episode: {
         thumbImg: 'https://www.preposterousuniverse.com/wp-content/uploads/mindscape-med.png',
         title: 'Solo -- Why Is There Something Rather than Nothing?',
+        currentPosition: 0,
+        episodeLength: 4800,
       },
       isPlaying: false,
       isExpanded: false,
@@ -17,6 +19,9 @@ class PlaybackCenter extends Component {
 
     this.togglePlay = this.togglePlay.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
+    this.backward = this.backward.bind(this);
+    this.forward = this.forward.bind(this);
   }
 
   togglePlay() {
@@ -33,6 +38,49 @@ class PlaybackCenter extends Component {
     });
   }
 
+  updatePosition(pos) {
+    const { episode } = this.state;
+
+    if (pos < 0) {
+      episode.currentPosition = 0;
+    } else if (pos > episode.episodeLength) {
+      episode.currentPosition = episode.episodeLength;
+    } else {
+      episode.currentPosition = pos;
+    }
+    this.setState({
+      ...episode,
+    });
+  }
+
+  forward() {
+    const { episode } = this.state;
+
+    if ((episode.currentPosition + 15) > episode.episodeLength) {
+      episode.currentPosition = episode.episodeLength;
+    } else {
+      episode.currentPosition += 15;
+    }
+
+    this.setState({
+      episode,
+    });
+  }
+
+  backward() {
+    const { episode } = this.state;
+
+    if ((episode.currentPosition - 15) < 0) {
+      episode.currentPosition = 0;
+    } else {
+      episode.currentPosition -= 15;
+    }
+
+    this.setState({
+      episode,
+    });
+  }
+
   render() {
     const {
       episode, isPlaying, isExpanded,
@@ -44,10 +92,27 @@ class PlaybackCenter extends Component {
           <button type="button" className={isExpanded ? 'fold' : 'expand'} onClick={this.toggleModal} />
         </div>
         { !isExpanded
-        && <PlaybackBar isPlaying={isPlaying} togglePlay={this.togglePlay} episode={episode} />
+        && (
+        <PlaybackBar
+          isPlaying={isPlaying}
+          togglePlay={this.togglePlay}
+          episode={episode}
+          updatePosition={this.updatePosition}
+          forward={this.forward}
+        />
+        )
         }
         { isExpanded
-          && <PlaybackModal isPlaying={isPlaying} togglePlay={this.togglePlay} episode={episode} />
+          && (
+          <PlaybackModal
+            isPlaying={isPlaying}
+            togglePlay={this.togglePlay}
+            episode={episode}
+            updatePosition={this.updatePosition}
+            backward={this.backward}
+            forward={this.forward}
+          />
+          )
         }
       </div>
     );
