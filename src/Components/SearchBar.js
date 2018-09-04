0b506abaec3ba3aa33bd27d921898e67eadeef4b
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-04 16:39:10 +0200

    replace register button with Link

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SearchBar = () => (
   <div className="searchbar">
     <input placeholder="Search..." />
     <div>
-      <button type="button">Register</button>
+      <Link to="/register">Register</Link>
     </div>
   </div>
 );
