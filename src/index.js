b5d233cc356c91ec72cc3ff2f54c99c8b690270f
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-06 01:28:39 +0200

    create store with thunk midleware to wrap app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import Reducer from './Reducers/AuthReducer';
 import './Assets/Styles/index.css';
 import Router from './Router';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+const store = createStore(Reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
