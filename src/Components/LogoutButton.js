d363168ec1691b8252f13c701c48995f5af6d5f7
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-06 01:29:55 +0200

    create logoutbutton

---
+++ src/Components/LogoutButton.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class LogoutButton extends Component {
+  constructor(props) {
+    super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    const { toggleModal, atemptLogout } = this.props;
+
+    atemptLogout(true);
+    toggleModal('menu');
+  }
+
+  render() {
+    const { isLogedIn } = this.props;
+
+    return isLogedIn ? (
+      <li><button type="button" className="logout-button" onClick={this.logout}>Logout</button></li>
+    ) : null;
+  }
+}
+LogoutButton.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  atemptLogout: PropTypes.func.isRequired,
+  isLogedIn: PropTypes.bool.isRequired,
+
+};
+export default LogoutButton;
