358e6818e4d271f2f39c3d3600c5c0197c0b8d8c
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-10 15:29:44 +0200

    add follows button when loggedin

--- src/Components/Search/SearchBar.js
+++ src/Components/Search/SearchBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => (
+const SearchBar = ({ isLogedIn }) => (
   <div className="searchbar">
     <input placeholder="Search..." />
     <div>
-      <Link to="/register">Register</Link>
+      { isLogedIn
+        ? <button type="button" className="follows" />
+        : <Link to="/register">Register</Link>
+      }
     </div>
   </div>
 );
+
+SearchBar.propTypes = {
+  isLogedIn: PropTypes.bool.isRequired,
+};
 
 export default SearchBar;
