343a394c3f2b1f4293caedf9e8c2565a296ad24a
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-10 02:02:25 +0200

    Update authreducer
    	* import User and Redirect Model
    	* add isRegistering, user and redirect to default state
    	* set user in case USER_LOGIN_SUCESS
    	* add cases for registering actiontypes

--- src/Reducers/AuthReducer.js
+++ src/Reducers/AuthReducer.js
@@ -1,9 +1,14 @@
 import ActionTypes from '../Actions/Auth/types';
+import User from '../Models/User';
+import Redirect from '../Models/Redirect';
 
 const DEFAULT_STATE = {
   isLogedIn: false,
   isLogingIn: false,
   isLogingOut: false,
+  isRegistering: false,
+  user: new User(),
+  redirect: new Redirect(),
 };
 
 export default function (state = DEFAULT_STATE, action) {
@@ -11,15 +16,25 @@ export default function (state = DEFAULT_STATE, action) {
     case ActionTypes.USER_LOGIN_START:
       return { ...state, isLogingIn: true };
     case ActionTypes.USER_LOGIN_SUCESS:
-      return { ...state, isLogedIn: true, isLoginIn: false };
+      return {
+        ...state, isLogedIn: true, isLoginIn: false, user: new User(action.user),
+      };
     case ActionTypes.USER_LOGIN_FAILUR:
       return { ...state, isLoginIn: false };
     case ActionTypes.USER_LOGOUT_START:
       return { ...state, isLogingOut: true };
     case ActionTypes.USER_LOGOUT_SUCESS:
-      return { ...state, isLogedIn: false, isLoginOut: false };
+      return {
+        ...state, isLogedIn: false, isLogingOut: false,
+      };
     case ActionTypes.USER_LOGOUT_FAILUR:
       return { ...state, isLogingOut: false };
+    case ActionTypes.USER_REGISTRATION_START:
+      return { ...state, isRegistering: true };
+    case ActionTypes.USER_REGISTRATION_SUCESS:
+      return { ...state, isRegistering: false, redirect: new Redirect({ to: '/' }) };
+    case ActionTypes.USER_REGISTRATION_FAILUR:
+      return { ...state, isRegistering: false };
     default:
       return { ...state };
   }
