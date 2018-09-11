acb448edfc74ea48f699e818bef951ad27738541
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-11 03:29:45 +0200

    add maping of dispatch for attemptsearch to searchBar also add more states to be maped to props

--- src/Containers/SearchBar.js
+++ src/Containers/SearchBar.js
@@ -1,10 +1,19 @@
 import { connect } from 'react-redux';
 import SearchBar from '../Components/Search/SearchBar';
+import { atemptSearch } from '../Actions/Search';
 
 function mapStateToProps(state) {
   return {
     isLogedIn: state.Auth.isLogedIn,
+    type: state.Search.type,
+    offset: state.Search.offset,
+    redirectToSearch: state.Search.redirectToSearch,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    search: query => dispatch(atemptSearch(query)),
   };
 }
 
-export default connect(mapStateToProps)(SearchBar);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
