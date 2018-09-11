b7958b49a534c8d3bcda19f9563855fa1ff3a365
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-11 03:24:51 +0200

    update fetch to only send body if it has been provided

--- src/Helpers/Fetch.js
+++ src/Helpers/Fetch.js
@@ -1,14 +1,25 @@
 import config from '../Config/config';
 
-export default async function fetchFromAPi(path, method, body) {
+export default async function fetchFromAPi(path, method, data) {
   const headers = new Headers({
     'Content-type': 'application/json',
   });
 
+  const body = Object.keys(data).length !== 0 ? data : undefined;
+
+  const options = {
+    method,
+    headers,
+  };
+
+  if (body) {
+    options.body = body;
+  }
+
   const response = await new Promise((resolve, reject) => {
-    fetch(`${config.API_BASE_URL}${path}`, { method, headers, body })
-      .then(data => data.json())
-      .then(data => resolve(data)).catch(error => reject(error));
+    fetch(`${config.API_BASE_URL}${path}`, options)
+      .then(res => res.json())
+      .then(res => resolve(res)).catch(error => reject(error));
   });
 
   return response;
