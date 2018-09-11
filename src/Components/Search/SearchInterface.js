1ce1e1d53d077b506e32027d06cd8ecdc60263b4
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-12 01:27:09 +0200

    import ad use pagination component in SearchInterface

--- src/Components/Search/SearchInterface.js
+++ src/Components/Search/SearchInterface.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchControlles from '../../Containers/SearchControlles';
 import SearchResluts from '../../Containers/SearchResults';
+import Pagination from '../../Containers/Pagination';
 
 const SearchInterface = () => (
   <div>
     <SearchControlles />
     <SearchResluts />
+    <Pagination />
   </div>
 );
 
