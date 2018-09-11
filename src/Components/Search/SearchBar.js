ee9ab2deaa399963dec87871628284b1a133f565
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-11 03:22:23 +0200

    Update SearchBar Component
    	 * rewrite component as a class insted of being a stateles component
          	 * add propsvalidation for new props
             * add form to wrap input to enable submit
             * add handle change function to update input value
             * create search function that passes some props and term from state to function recived thru store
             * use Shuold component update to prevent Redirect from being redered twice
            * add Redirect component to redirect to search page if not allready thea
    re

--- src/Components/Search/SearchBar.js
+++ src/Components/Search/SearchBar.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const SearchBar = ({ isLogedIn }) => (
-  <div className="searchbar">
-    <input placeholder="Search..." />
-    <div>
-      { isLogedIn
-        ? <button type="button" className="follows" />
-        : <Link to="/register">Register</Link>
-      }
-    </div>
-  </div>
-);
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    };
+
+    this.search = this.search.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  search(e) {
+    e.preventDefault();
+    const {
+      type, offset, search, path,
+    } = this.props;
+    const { term } = this.state;
+
+    search({
+      term, type, offset, path,
+    });
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { isLogedIn, redirectToSearch, path } = this.props;
+    const { term } = this.state;
+
+    return (
+      <div className="searchbar">
+        {redirectToSearch && path !== '/search' ? <Redirect to="/search" /> : null}
+        <form onSubmit={this.search}>
+          <input placeholder="Search..." name="term" value={term} onChange={this.handleChange} />
+        </form>
+        <div>
+          { isLogedIn
+            ? <button type="button" className="follows" />
+            : <Link to="/register">Register</Link>
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 SearchBar.propTypes = {
   isLogedIn: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
+  offset: PropTypes.number.isRequired,
+  search: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
+  redirectToSearch: PropTypes.bool.isRequired,
 };
 
 export default SearchBar;
