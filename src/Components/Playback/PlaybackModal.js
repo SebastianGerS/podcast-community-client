f083c74635cdf0eefb623eb3e09ad4042e99d300
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-17 19:12:30 +0200

    Update PlaybackModal with new props and fallbacks for when no episode has been selected
    	* add pos and getDuration to props recived
    	* add fallbacks for title and image
    	* import Episode model
    	* update propvalidation
    	* rename updatePosition to seek

--- src/Components/Playback/PlaybackModal.js
+++ src/Components/Playback/PlaybackModal.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ProgressBar from './ProgressBar';
+import Episode from '../../Models/Episode';
 
 const PlaybackModal = ({
-  episode, isPlaying, togglePlay, updatePosition, backward, forward,
-}) => (
-  <div className="playbackmodal">
-    <figure>
-      <img src={episode.thumbImg} alt="podcast-img" className="podcast-img" />
-    </figure>
-    <ProgressBar episode={episode} updatePosition={updatePosition} />
-    <p>{episode.title}</p>
-    <div className="playback-controles">
-      <button type="button" className="backward" onClick={backward} />
-      <button type="button" className={isPlaying ? 'pause' : 'play'} onClick={togglePlay} />
-      <button type="button" className="forward" onClick={forward} />
+  episode, isPlaying, togglePlay, seek, backward, forward, pos, getDuration,
+}) => {
+  const title = typeof episode.title_original === 'string' ? episode.title_original : '';
+  const image = typeof episode.image === 'string' ? episode.image : 'https://via.placeholder.com/375x375';
+
+  return (
+    <div className="playbackmodal">
+      <figure>
+        <img src={image} alt="podcast-img" className="podcast-img" />
+      </figure>
+      <ProgressBar episode={episode} seek={seek} pos={pos} getDuration={getDuration} />
+      <p>{title}</p>
+      <div className="playback-controles">
+        <button type="button" className="backward" onClick={backward} />
+        <button type="button" className={isPlaying ? 'pause' : 'play'} onClick={togglePlay} />
+        <button type="button" className="forward" onClick={forward} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 PlaybackModal.propTypes = {
-  episode: PropTypes.shape({
-    thumbImg: PropTypes.string,
-    title: PropTypes.string,
-    currentPosition: PropTypes.number,
-    episodeLength: PropTypes.number,
-  }).isRequired,
+  episode: PropTypes.shape(Episode).isRequired,
   isPlaying: PropTypes.bool.isRequired,
   togglePlay: PropTypes.func.isRequired,
-  updatePosition: PropTypes.func.isRequired,
+  seek: PropTypes.func.isRequired,
   forward: PropTypes.func.isRequired,
   backward: PropTypes.func.isRequired,
+  pos: PropTypes.number.isRequired,
+  getDuration: PropTypes.func.isRequired,
 };
 
 export default PlaybackModal;
