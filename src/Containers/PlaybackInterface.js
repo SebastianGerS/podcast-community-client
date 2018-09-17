84cdcf883ceabfc17e39a4cd491ab4875a04c9a5
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-17 18:37:23 +0200

    add connection to player reducer and player related actions for PlaybackInterface

--- src/Containers/PlaybackInterface.js
+++ src/Containers/PlaybackInterface.js
@@ -1,17 +1,24 @@
 import { connect } from 'react-redux';
 import PlaybackInterface from '../Components/Playback/PlaybackInterface';
 import { togglePlaybackKModal } from '../Actions/Modal';
+import { play, stop } from '../Actions/Player';
+
 
 function mapStateToProps(state) {
   return {
     menuIsActive: state.Modal.menuIsActive,
     modalIsActive: state.Modal.playbackModalIsActive,
+    episode: state.Player.episode,
+    isPlaying: state.Player.isPlaying,
+    startEpisode: state.Player.startEpisode,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     toggleModal: () => dispatch(togglePlaybackKModal()),
+    play: () => dispatch(play()),
+    stop: () => dispatch(stop()),
   };
 }
 
