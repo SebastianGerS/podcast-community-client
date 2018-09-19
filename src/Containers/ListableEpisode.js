23f5658d64c811e9a892098bc47cb90be01d3411
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-19 23:33:20 +0200

    add isDowloading state and dowload action to ListableEpisode component

--- src/Containers/ListableEpisode.js
+++ src/Containers/ListableEpisode.js
@@ -1,17 +1,19 @@
 import { connect } from 'react-redux';
 import ListableEpisode from '../Components/Search/ListableEpisode';
-import { setAudio, stop } from '../Actions/Player';
+import { setAudio, stop, download } from '../Actions/Player';
 
 function mapStateToProps(state) {
   return {
     episode: state.Player.episode,
     isPlaying: state.Player.isPlaying,
+    isDownloading: state.Player.isDownloading,
   };
 }
 function mapDispatchToProps(dispatch) {
   return {
     setAudio: episode => dispatch(setAudio(episode)),
     stop: () => dispatch(stop()),
+    download: episode => dispatch(download(episode)),
   };
 }
 
