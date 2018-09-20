bebae86a84fa8d064f4e84d1f4a75b4e8a8f4c4e
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-20 17:59:03 +0200

    add link to listable user to alow linking to userprofiles

--- src/Components/Search/ListableUser.js
+++ src/Components/Search/ListableUser.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import User from '../../Models/User';
 
 const ListableUser = ({ data }) => (
   <div className="listable-user">
     <button aria-label="follow-button" type="button" />
     <div>
-      <figure>
-        <img src={data.profile_img.thumb} alt="podcastlogo" />
-      </figure>
-      <p>{data.username}</p>
+      <Link to={`/profile/${data._id}`}>
+        <figure>
+          <img src={data.profile_img.thumb} alt="podcastlogo" />
+        </figure>
+        <p>{data.username}</p>
+      </Link>
     </div>
   </div>
 );
