0c39bf5be8ce70776a2c61d5f0ee8df80092e89d
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-20 18:12:09 +0200

    create actions and actiontypes to get and update users

---
+++ src/Actions/User/index.js
@@ -0,0 +1,63 @@
+import ActionTypes from './types';
+import { Fetch, formatError } from '../../Helpers/Fetch';
+import { atemptSetMessage } from '../Message';
+
+export const startUpdateUser = () => (
+  { type: ActionTypes.UPDATE_USER_START }
+);
+export const UserUpdated = () => (
+  {
+    type: ActionTypes.UPDATE_USER_SUCCESS,
+  }
+);
+
+export const userUpdateFailure = () => (
+  { type: ActionTypes.UPDATE_USER_FAILUR }
+);
+
+export const startGettingUser = () => (
+  { type: ActionTypes.GET_USER_START }
+);
+export const gotUser = user => (
+  {
+    type: ActionTypes.GET_USER_SUCCESS,
+    user,
+  }
+);
+
+export const getUserFailure = () => (
+  { type: ActionTypes.GET_USER_FAILUR }
+);
+
+const updateUser = (id, body) => Fetch(`/users/${id}`, 'POST', body);
+
+const getUser = id => Fetch(`/users/${id}`, 'GET', {});
+
+
+export const atemptGetUser = id => async (dispatch) => {
+  dispatch(startGettingUser());
+
+  const response = await getUser(id).catch(error => error);
+
+  if (response.message === 'Failed to fetch') dispatch(atemptSetMessage({ message: 'unable to connect to resource pleas check your internet conection', type: 'error' }));
+
+  if (response.error) {
+    dispatch(getUserFailure());
+
+    dispatch(atemptSetMessage({ message: response.error.errmsg, type: 'info' }));
+  }
+  if (response.user) dispatch(gotUser(response.user));
+};
+
+export const atemptUpdateUser = ({ _id, body }) => async (dispatch) => {
+  dispatch(startUpdateUser());
+  const response = await updateUser(_id, body).catch(error => error);
+  if (response.message === 'Failed to fetch') dispatch(atemptSetMessage({ message: 'unable to connect to resource pleas check your internet conection', type: 'error' }));
+
+  if (response.error) {
+    dispatch(userUpdateFailure());
+
+    dispatch(atemptSetMessage({ message: formatError(response.error.errmsg), type: 'info' }));
+  }
+  if (response.user) dispatch(UserUpdated()); dispatch(atemptGetUser(_id));
+};
