addf88bb3322d3188dffed3ee4b8e6005be3ac6c
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-22 03:20:10 +0200

    add validation for loginform

--- src/Containers/LoginForm.js
+++ src/Containers/LoginForm.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import LoginForm from '../Components/Auth/LoginForm';
 import { atemptLogin } from '../Actions/Auth';
+import { atemptSetMessage } from '../Actions/Message';
 
 function mapDispatchToProps(dispatch) {
   return {
     atemptLogin: data => dispatch(atemptLogin(data)),
+    atemptSetMessage: data => dispatch(atemptSetMessage(data)),
   };
 }
 
