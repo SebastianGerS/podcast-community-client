f5cebb0a245854d5d9cceb3aa72689c59d7de955
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-25 11:57:16 +0200

    create listablePodcast component to be used by Folder

---
+++ src/Components/Subscription/ListablePodcast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Podcast from '../../Models/Podcast';
+import { getDatefromMilisecond } from '../../Helpers/Time';
+
+const ListablePodcast = ({ data }) => (
+  <div className="listable-podcast">
+    <figure>
+      <img src={data.image} alt="podcastlogo" className="podcast-logo" />
+      <figcaption>
+        <span>{getDatefromMilisecond(data.lastest_pub_date_ms)}</span>
+      </figcaption>
+    </figure>
+  </div>
+);
+
+ListablePodcast.propTypes = {
+  data: PropTypes.shape(Podcast).isRequired,
+};
+
+export default ListablePodcast;
