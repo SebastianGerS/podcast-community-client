ce0bfe0325dc2549a8b353002d2ff8eb9dbb30bd
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-25 16:30:33 +0200

    Add func to enable updating user password and type
    	* add validation for userpassword
    	* add func to update password and type
    	* add validation for props

--- src/Components/Settings/UserSettings.js
+++ src/Components/Settings/UserSettings.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import JWT from 'jsonwebtoken';
+import config from '../../Config/config';
+import User from '../../Models/User';
 
 class UserSettings extends Component {
   constructor(props) {
@@ -7,8 +11,30 @@ class UserSettings extends Component {
       type: '',
       password: '',
       passwordConfirmation: '',
+
     };
     this.handleChange = this.handleChange.bind(this);
+    this.updateUser = this.updateUser.bind(this);
+  }
+
+  componentWillMount() {
+    const { user } = this.props;
+
+    this.setState({
+      type: user.type,
+    });
+  }
+
+  shouldComponentUpdate(nextProps) {
+    const { user } = this.props;
+
+    if (nextProps.user !== user) {
+      this.setState({
+        type: nextProps.user.type,
+      });
+      return true;
+    }
+    return true;
   }
 
   handleChange(e) {
@@ -19,13 +45,44 @@ class UserSettings extends Component {
     });
   }
 
+  validateUserData() {
+    const {
+      password, passwordConfirmation,
+    } = this.state;
+    const { atemptSetMessage } = this.props;
+
+    if (password !== passwordConfirmation) {
+      atemptSetMessage({ message: 'Passwordconfirmation does not match', type: 'warning' });
+      return false;
+    }
+    if (password.length < 8) {
+      atemptSetMessage({ message: 'passwords must be atleast 8 characters long', type: 'warning' });
+      return false;
+    }
+
+    return true;
+  }
+
+  updateUser(e) {
+    e.preventDefault();
+    const { updateUser, user } = this.props;
+    const { password, type } = this.state;
+
+    if (type !== user.type) {
+      updateUser(user._id, { type });
+    } else if (this.validateUserData()) {
+      const tempToken = JWT.sign(password, config.JWT_SECRET);
+      updateUser(user._id, { password: tempToken });
+    }
+  }
+
   render() {
     const { type, password, passwordConfirmation } = this.state;
     /* eslint-disable jsx-a11y/label-has-associated-control, jsx-a11y/label-has-for */
     return (
       <div className="user-settings">
         <h3>Change Type</h3>
-        <form>
+        <form onSubmit={this.updateUser}>
           <label htmlFor="type">
         UserType:
             <select name="type" id="type" onChange={this.handleChange} value={type}>
@@ -36,21 +93,26 @@ class UserSettings extends Component {
           <button type="submit">Update</button>
         </form>
         <h3>Change Password</h3>
-        <form>
+        <form onSubmit={this.updateUser}>
           <label htmlFor="password">
-          Password:
-            <input name="password" id="password" onChange={this.handleChange} value={password} />
+        Password:
+            <input type="password" name="password" id="password" className={password.length < 8 ? 'invalid' : 'valid'} value={password} onChange={this.handleChange} />
           </label>
           <label htmlFor="passwordConfirmation">
-          PasswordConfirmation:
-            <input name="passwordConfirmation" id="passwordConfirmation " onChange={this.handleChange} value={passwordConfirmation} />
+        Password Confirmation:
+            <input type="password" name="passwordConfirmation" id="passwordConfirmation" className={passwordConfirmation.length < 8 || passwordConfirmation !== password ? 'invalid' : 'valid'} value={passwordConfirmation} onChange={this.handleChange} />
           </label>
-
           <button type="submit">Update</button>
         </form>
       </div>
     );
   }
 }
+
+UserSettings.propTypes = {
+  user: PropTypes.shape(User).isRequired,
+  updateUser: PropTypes.func.isRequired,
+  atemptSetMessage: PropTypes.func.isRequired,
+};
 
 export default UserSettings;
