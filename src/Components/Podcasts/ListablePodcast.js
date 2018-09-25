0bc7f1b931cf75520ed1def323e3bd21719cdaad
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-25 10:45:40 +0200

    update keys to use on the data object to conform with update in TopPodcasts

--- src/Components/Podcasts/ListablePodcast.js
+++ src/Components/Podcasts/ListablePodcast.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Podcast from '../../Models/Podcast';
+import { getDatefromMilisecond } from '../../Helpers/Time';
 
 const ListablePodcast = ({ data }) => (
   <div className="listable-podcast">
     <figure>
-      <img src={data.logo} alt="podcastlogo" />
+      <img src={data.image} alt="podcastlogo" className="podcast-logo" />
       <figcaption>
-        {data.podcast}
-        <span>{data.lastestEpisodeDate}</span>
+        {data.title}
+        <span>{getDatefromMilisecond(data.lastest_pub_date_ms)}</span>
       </figcaption>
     </figure>
   </div>
 );
 
 ListablePodcast.propTypes = {
-  data: PropTypes.objectOf(PropTypes.string).isRequired,
+  data: PropTypes.shape(Podcast).isRequired,
 };
 
 export default ListablePodcast;
