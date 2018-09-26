2a505672efc8b33a0f00a0ec088c12b433667145
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-26 20:03:22 +0200

    update calculations du to incressed width of the progressbar

--- src/Helpers/UserAgent.js
+++ src/Helpers/UserAgent.js
@@ -1,6 +1,6 @@
 export const getProgressbarLength = () => ((window.innerWidth / 10) * 9);
 
-export const getStartingPoint = () => ((window.innerWidth - getProgressbarLength()) / 2 + 10);
+export const getStartingPoint = () => ((window.innerWidth - getProgressbarLength()) / 2 + 20);
 
 export const getSecondsPerPixel = episodeLength => (episodeLength / (getProgressbarLength() - 20));
 
