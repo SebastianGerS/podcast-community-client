a98ee514d3b2faf21dbb24069ad4cfaa59bf39a2
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-27 23:22:44 +0200

    Creat func get device innerwidth and save to store
    	* create actiontype for seting device height
    	* create action to set height
    	* create action to add  eventlistener on resize and trigger set hight in store when reiszed
    	* add case for set height in modalreducer

--- src/Actions/Modal/index.js
+++ src/Actions/Modal/index.js
@@ -10,3 +10,12 @@ export const togglePlaybackKModal = () => ({
 export const toggleMenu = () => ({
   type: actionTypes.TOGGLE_MENU,
 });
+export const setHeight = height => ({
+  type: actionTypes.SET_HEIGHT,
+  height,
+});
+export const checkIfResized = () => (dispatch) => {
+  window.addEventListener('resize', () => {
+    dispatch(setHeight(window.innerHeight));
+  });
+};
--- src/Reducers/ModalReducer.js
+++ src/Reducers/ModalReducer.js
@@ -4,27 +4,36 @@ const DEFAULT_STATE = {
   loginModalIsActive: false,
   playbackModalIsActive: false,
   menuIsActive: false,
+  height: 0,
 };
 
 export default function (state = DEFAULT_STATE, action) {
   switch (action.type) {
     case ActionTypes.TOGGLE_LOGIN_MODAL:
       return {
+        ...state,
         loginModalIsActive: !state.loginModalIsActive,
         playbackModalIsActive: false,
         menuIsActive: false,
       };
     case ActionTypes.TOGGLE_PLAYBACK_MODAL:
       return {
+        ...state,
         loginModalIsActive: false,
         playbackModalIsActive: !state.playbackModalIsActive,
         menuIsActive: false,
       };
     case ActionTypes.TOGGLE_MENU:
       return {
+        ...state,
         loginModalIsActive: false,
         playbackModalIsActive: false,
         menuIsActive: !state.menuIsActive,
+      };
+    case ActionTypes.SET_HEIGHT:
+      return {
+        ...state,
+        height: action.height,
       };
     default:
       return { ...state };
