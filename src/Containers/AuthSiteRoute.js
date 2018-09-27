ffb510eddd869528edfb105ee6defb00111e2cbe
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-09-27 23:29:40 +0200

    Add new props
    	* add height from Modal reducer
    	* add setHeight and checkIfResized

--- src/Containers/AuthSiteRoute.js
+++ src/Containers/AuthSiteRoute.js
@@ -1,17 +1,21 @@
 import { connect } from 'react-redux';
 import AuthSiteRoute from '../Helpers/AuthSiteRoute';
 import { checkIfLogedIn } from '../Actions/Auth';
+import { checkIfResized, setHeight } from '../Actions/Modal';
 
 function mapStateToProps(state) {
   return {
     isLogedIn: state.Auth.isLogedIn,
     menuIsActive: state.Modal.menuIsActive,
+    height: state.Modal.height,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     checkIfLogedIn: () => dispatch(checkIfLogedIn()),
+    checkIfResized: () => dispatch(checkIfResized()),
+    setHeight: height => dispatch(setHeight(height)),
   };
 }
 
