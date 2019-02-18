703b6f75c84eee76c7f2b97f8b36d7a8854f7a1a
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-02-18 21:52:44 +0100

    Rework message animation
    * enable interupting the animation when a new message is queued
    
    Issue #4

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MessageModel from '../../Models/Message';
 
-const Message = ({ message }) => (
-  <div className={`message ${message.type}`}>
+const Message = ({ message, animation }) => (
+  <div className={`message ${message.type} ${animation} `}>
     <p>{message.text}</p>
   </div>
 );
 
 Message.propTypes = {
   message: PropTypes.shape(MessageModel).isRequired,
+  animation: PropTypes.string.isRequired,
 };
 
 export default Message;
