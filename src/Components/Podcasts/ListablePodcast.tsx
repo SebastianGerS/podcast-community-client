e28e0c7fea198d1f77234d74b237323ee9aa2654
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-03-19 23:47:11 +0100

    add link to individual podcast view

--- src/Components/Podcasts/ListablePodcast.tsx
+++ src/Components/Podcasts/ListablePodcast.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Podcast } from '../../Models/Podcast';
 import { getDatefromMilisecond } from '../../Helpers/Time';
 
@@ -7,17 +8,19 @@ interface Props {
 }
 
 const ListablePodcast = ({ data }: Props): JSX.Element => (
-  <div className="listable-podcast">
-    <figure>
-      <img src={typeof data.image === 'string' ? data.image : ''} alt="podcastlogo" className="podcast-logo" />
-      <figcaption>
-        {data.title}
-        <span>
-          {getDatefromMilisecond(typeof data.lastest_pub_date_ms === 'number' ? data.lastest_pub_date_ms : 0)}
-        </span>
-      </figcaption>
-    </figure>
-  </div>
+  <Link to={`/podcasts/${typeof data.id === 'string' ? data.id : ''}`}>
+    <div className="listable-podcast">
+      <figure>
+        <img src={typeof data.image === 'string' ? data.image : ''} alt="podcastlogo" className="podcast-logo" />
+        <figcaption>
+          {data.title}
+          <span>
+            {getDatefromMilisecond(typeof data.lastest_pub_date_ms === 'number' ? data.lastest_pub_date_ms : 0)}
+          </span>
+        </figcaption>
+      </figure>
+    </div>
+  </Link>
 );
 
 export default ListablePodcast;
