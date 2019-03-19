362dda0385fca68dafe2dc42aa6a3bfbc73fc294
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-03-19 23:40:09 +0100

    Create Actions for fetching individual podcast
    * Create actions to fecth individual podcast
    * Move TopPodcasts actions to new subdirectory
    * update PodcastReducer with cases for new actiontypes and new key for podcast
    
    Issue #12

---
+++ src/Actions/Podcast/Podcast/index.ts
@@ -0,0 +1,64 @@
+import { Dispatch } from 'redux';
+import * as ActionTypes from './types';
+import { Fetch, Response } from '../../../Helpers/Fetch';
+import { atemptSetMessage, SetMessage } from '../../Message';
+import { Podcast } from '../../../Models/Podcast';
+
+interface GetPodcastStart {
+  type: ActionTypes.GET_PODCAST_START;
+}
+
+const startGetPodcast = (): GetPodcastStart => ({
+  type: ActionTypes.GET_PODCAST_START,
+});
+
+interface GetPodcastSuccess {
+  type: ActionTypes.GET_PODCAST_SUCCESS;
+  podcast: Podcast;
+}
+
+const gotPodcast = (podcast: Podcast): GetPodcastSuccess => ({
+  type: ActionTypes.GET_PODCAST_SUCCESS,
+  podcast,
+});
+
+interface GetPodcastFailure {
+  type: ActionTypes.GET_PODCAST_FAILURE;
+}
+
+const getPodcastFailure = (): GetPodcastFailure => ({
+  type: ActionTypes.GET_PODCAST_FAILURE,
+});
+
+const getPodcast = (podcastId: string): Promise<Response> => Fetch(`/podcasts/${podcastId}`, 'GET', {});
+
+export type GetPodcastAction = GetPodcastStart | GetPodcastSuccess |GetPodcastFailure;
+
+type AtemptGetPodcastAction = (dispatch: Dispatch<GetPodcastAction | SetMessage>) => Promise<void>;
+
+export const atemptGetPodcast = (podcastId: string): AtemptGetPodcastAction => async (
+  dispatch: Dispatch<GetPodcastAction | SetMessage>,
+): Promise<void> => {
+  dispatch(startGetPodcast());
+
+  const response = await getPodcast(podcastId).catch(error => error);
+  console.log(response);
+  if (response.message === 'Failed to fetch') {
+    atemptSetMessage(
+      {
+        text: 'unable to connect to resource pleas check your internet conection',
+        type: 'error',
+      },
+    )(dispatch);
+  }
+
+  if (response.error) {
+    dispatch(getPodcastFailure());
+
+    atemptSetMessage({ text: response.error.errmsg, type: 'info' })(dispatch);
+  }
+
+  if (response.length !== 0 && !response.error && !response.message) {
+    dispatch(gotPodcast(response));
+  }
+};
--- src/Reducers/PodcastReducer.tsx
+++ src/Reducers/PodcastReducer.tsx
@@ -1,17 +1,19 @@
 import * as ActionTypes from '../Actions/Podcast/types';
 import { Podcast } from '../Models/Podcast';
-import { GetTopPodcastsAction } from '../Actions/Podcast';
+import { PodcastActions } from '../Actions/Podcast';
 
 export interface PodcastState {
   isFetching: boolean;
   topPodcasts: Podcast[];
+  podcast: Podcast;
 }
 const DEFAULT_STATE: PodcastState = {
   isFetching: false,
   topPodcasts: [new Podcast()],
+  podcast: new Podcast(),
 };
 
-export default function (state: PodcastState = DEFAULT_STATE, action: GetTopPodcastsAction): PodcastState {
+export default function (state: PodcastState = DEFAULT_STATE, action: PodcastActions): PodcastState {
   switch (action.type) {
     case ActionTypes.GET_TOP_PODCASTS_START:
       return { ...state, isFetching: true };
@@ -22,6 +24,18 @@ export default function (state: PodcastState = DEFAULT_STATE, action: GetTopPodc
         topPodcasts: action.podcasts.map(podcast => new Podcast(podcast)),
       };
     case ActionTypes.GET_TOP_PODCASTS_FAILURE:
+      return {
+        ...state, isFetching: false,
+      };
+    case ActionTypes.GET_PODCAST_START:
+      return { ...state, isFetching: true };
+    case ActionTypes.GET_PODCAST_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        podcast: new Podcast(action.podcast),
+      };
+    case ActionTypes.GET_PODCAST_FAILURE:
       return {
         ...state, isFetching: false,
       };
