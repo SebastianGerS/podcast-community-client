991d5585c94d97bab5359bbf4aba8963e1c6a0e1
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-03-20 16:38:18 +0100

    Remove usued key from Episode and Podcast models

--- src/Models/Episode.tsx
+++ src/Models/Episode.tsx
@@ -2,52 +2,38 @@ import Immutable from 'immutable';
 
 export interface Episode {
   description_original: string | StringConstructor;
-  description: string | StringConstructor;
-  publisher_highlighted: string | StringConstructor;
   podcast_listennotes_url: string | StringConstructor;
-  description_highlighted: string | StringConstructor;
   podcast_title_original: string | StringConstructor;
-  transcripts_highlighted: string[] | ArrayConstructor;
   rss: string | StringConstructor;
   audio_length: string | StringConstructor;
   pub_date_ms: number | NumberConstructor;
   thumbnail: string | StringConstructor;
   listennotes_url: string | StringConstructor;
-  podcast_title_highlighted: string | StringConstructor;
   image: string | StringConstructor;
   publisher_original: string | StringConstructor;
   genres: string[] | ArrayConstructor;
-  title_highlighted: string | StringConstructor;
   podcast_id: string | StringConstructor;
   audio: string | StringConstructor;
   id: string | StringConstructor;
   title_original: string | StringConstructor;
-  title: string | StringConstructor;
   itunes_id: number | NumberConstructor;
 }
 
 export const Episode = Immutable.Record<Episode>({
   description_original: String,
-  description: String,
-  publisher_highlighted: String,
   podcast_listennotes_url: String,
-  description_highlighted: String,
   podcast_title_original: String,
-  transcripts_highlighted: Array,
   rss: String,
   audio_length: String,
   pub_date_ms: Number,
   thumbnail: String,
   listennotes_url: String,
-  podcast_title_highlighted: String,
   image: String,
   publisher_original: String,
   genres: Array,
-  title_highlighted: String,
   podcast_id: String,
   audio: String,
   id: String,
   title_original: String,
-  title: String,
   itunes_id: Number,
 });
--- src/Models/Podcast.tsx
+++ src/Models/Podcast.tsx
@@ -1,44 +1,35 @@
 import Immutable from 'immutable';
-import { Episode } from './Episode';
 
 export interface Podcast {
   description: string | StringConstructor;
   description_original: string | StringConstructor;
   publisher: string | StringConstructor;
-  publisher_highlighted: string | StringConstructor;
   lastest_pub_date_ms: number | NumberConstructor;
-  description_highlighted: string | StringConstructor;
   rss: string | StringConstructor;
   thumbnail: string | StringConstructor;
   image: string | StringConstructor;
   publisher_original: string | StringConstructor;
   listennotes_url: string | StringConstructor;
-  title_highlighted: string | StringConstructor;
   genres: string[] | ArrayConstructor;
   title: string | StringConstructor;
   title_original: string | StringConstructor;
   id: string | StringConstructor;
   itunes_id: number | NumberConstructor;
-  episodes: Episode[] | ArrayConstructor;
 }
 
 export const Podcast = Immutable.Record<Podcast>({
   description: String,
   description_original: String,
   publisher: String,
-  publisher_highlighted: String,
   lastest_pub_date_ms: Number,
-  description_highlighted: String,
   rss: String,
   thumbnail: String,
   image: String,
   publisher_original: String,
   listennotes_url: String,
-  title_highlighted: String,
   genres: Array,
   title: String,
   title_original: String,
   id: String,
   itunes_id: Number,
-  episodes: Array,
 });
