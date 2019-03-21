54b5bc61c9aabd5f526fa27d563f70c1493d28f7
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-03-21 21:46:52 +0100

    Moved infobox markup to new reusable component

--- src/Components/Search/ListableEpisode.tsx
+++ src/Components/Search/ListableEpisode.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Star from '../../Assets/Icons/star.svg';
 import { getDatefromMilisecond, getSecondsFromTimeString } from '../../Helpers/Time';
 import DownloadButton from '../../Containers/Common/DownloadButton';
 import { Episode } from '../../Models/Episode';
 import MoreOptionsButton from '../Common/MoreOptionsButton';
 import PlayButton from '../../Containers/Common/PlayButton';
+import InfoBox from '../Common/InfoBox';
 
 interface Props {
   data: Episode;
@@ -15,43 +17,40 @@ function ListableEpisode({ data }: Props): JSX.Element {
   const podcastTitle = typeof data.podcast_title_original === 'string' ? data.podcast_title_original : '';
   const publisher = typeof data.publisher_original === 'string' ? data.publisher_original : '';
   const description = typeof data.description_original === 'string' ? data.description_original : '';
+  const episodeId = typeof data.id === 'string' ? data.id : '';
+  const epiosdeLength = typeof data.audio_length === 'string'
+    ? `${Math.round(getSecondsFromTimeString(data.audio_length) / 60)} min`
+    : 'unknown';
 
   return (
     <div className="listable-episode">
-      <h3>{episodeTitle.length > 35 ? `${episodeTitle.substring(0, 31)}...` : episodeTitle}</h3>
-      <div>
-        <figure>
-          <img src={typeof data.thumbnail === 'string' ? data.thumbnail : ''} alt="podcastlogo" />
-        </figure>
-        <p>
-          <span>{podcastTitle.length > 30 ? `${podcastTitle.substring(0, 26)}...` : podcastTitle}</span>
-          <span>{`By ${publisher.length > 27 ? `${publisher.substring(0, 23)}...` : publisher}`}</span>
-          <span>
-            {`Relseed: ${typeof data.pub_date_ms === 'number' ? getDatefromMilisecond(data.pub_date_ms) : '—'}`}
-          </span>
-        </p>
-      </div>
-      <div>
-        <figure className="info-box">
-          <img src={Star} alt="star" />
-          <figcaption>5.0</figcaption>
-        </figure>
-        <div className="info-box">
+      <Link to={`/episodes/${episodeId}`}>
+        <h3 className="listable-episode-title">
+          {episodeTitle.length > 35 ? `${episodeTitle.substring(0, 31)}...` : episodeTitle}
+        </h3>
+        <div className="listable-episode-info">
+          <figure>
+            <img src={typeof data.thumbnail === 'string' ? data.thumbnail : ''} alt="podcastlogo" />
+          </figure>
           <p>
-            {
-              typeof data.audio_length === 'string'
-                ? `${Math.round(getSecondsFromTimeString(data.audio_length) / 60)} min`
-                : 'unknown'
-            }
+            <span>{podcastTitle.length > 30 ? `${podcastTitle.substring(0, 26)}...` : podcastTitle}</span>
+            <span>{`By ${publisher.length > 27 ? `${publisher.substring(0, 23)}...` : publisher}`}</span>
+            <span>
+              {`Relseed: ${typeof data.pub_date_ms === 'number' ? getDatefromMilisecond(data.pub_date_ms) : '—'}`}
+            </span>
           </p>
         </div>
-      </div>
-      <div>
-        <p>
-          {description.length > 150 ? `${description.substring(0, 147)}...` : description}
-        </p>
-      </div>
-      <div>
+        <div className="listable-episode-info-boxes">
+          <InfoBox text="5.0" icon={Star} alt="star" />
+          <InfoBox text={epiosdeLength} />
+        </div>
+        <div className="listable-episode-description">
+          <p>
+            {description.length > 150 ? `${description.substring(0, 147)}...` : description}
+          </p>
+        </div>
+      </Link>
+      <div className="listable-episode-controls">
         <DownloadButton episode={data} />
         <PlayButton episode={data} />
         <MoreOptionsButton />
