82f518668b6f6cbc430db856c420a4d6dc5e4125
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-03-21 22:04:30 +0100

    Create EpisodeReducer and actions
    * Create new EpisodeReducer
    * Add actions to fetch episode
    * Add func to redirect to a 404 page if episode is not found
    * Add connect Episode component to redux store
    
    Issue #13

--- src/Models/Episode.ts
+++ src/Models/Episode.ts
@@ -2,10 +2,11 @@ import Immutable from 'immutable';
 
 export interface Episode {
   description_original: string | StringConstructor;
+  description: string | StringConstructor;
   podcast_listennotes_url: string | StringConstructor;
   podcast_title_original: string | StringConstructor;
   rss: string | StringConstructor;
-  audio_length: string | StringConstructor;
+  audio_length: string | number | StringConstructor | NumberConstructor;
   pub_date_ms: number | NumberConstructor;
   thumbnail: string | StringConstructor;
   listennotes_url: string | StringConstructor;
@@ -16,15 +17,17 @@ export interface Episode {
   audio: string | StringConstructor;
   id: string | StringConstructor;
   title_original: string | StringConstructor;
+  title: string | StringConstructor;
   itunes_id: number | NumberConstructor;
 }
 
 export const Episode = Immutable.Record<Episode>({
   description_original: String,
+  description: String,
   podcast_listennotes_url: String,
   podcast_title_original: String,
   rss: String,
-  audio_length: String,
+  audio_length: String || Number,
   pub_date_ms: Number,
   thumbnail: String,
   listennotes_url: String,
@@ -35,5 +38,6 @@ export const Episode = Immutable.Record<Episode>({
   audio: String,
   id: String,
   title_original: String,
+  title: String,
   itunes_id: Number,
 });
