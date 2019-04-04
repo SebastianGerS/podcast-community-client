9a7477299ab8153a3e8dfb4aff57d8c16deb8588
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-04 16:21:09 +0200

    Add optional param for /follows and update notification links
    * add optional param for /follows to enable selecting followers tab directly
    * update links so that request notifications links to /follows/1 (followers tab) and other notifications links to agent users profile page
    
    Issue #18

--- src/Components/Notifications/Notification.tsx
+++ src/Components/Notifications/Notification.tsx
@@ -16,19 +16,24 @@ function NotificationComponent({
   const thumbnail = data.event.agent.item.profile_img ? data.event.agent.item.profile_img.thumb : '';
 
   let message;
+  let linkTo;
 
   switch (data.event.type) {
     case 'request':
       message = `${username} wants to follow you`;
+      linkTo = '/follows/1';
       break;
     case 'confirm':
       message = `you are now following ${username}`;
+      linkTo = `/profile/${data.event.agent.item._id}`;
       break;
     case 'follow':
       message = `${username} is now following you`;
+      linkTo = `/profile/${data.event.agent.item._id}`;
       break;
     default:
       message = '';
+      linkTo = '';
       break;
   }
 
@@ -47,7 +52,7 @@ function NotificationComponent({
         type="button"
         onClick={() => deleteNotification(data)}
       />
-      <Link to={`/profile/${data.event.agent.item._id}`} onClick={update}>
+      <Link to={linkTo} onClick={update}>
         <div>
           <figure>
             <img src={typeof thumbnail === 'string' ? thumbnail : ''} alt="profile_img" />
