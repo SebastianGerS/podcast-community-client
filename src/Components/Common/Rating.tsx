0896ede24a53931b68f2ffbf6012b822673f532f
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-09 10:48:54 +0200

    Update components to handle numbers
    * Add new prop for rating to rating componnent
    * Add possibility to pass numbers to InfoBox component

--- src/Components/Common/Rating.tsx
+++ src/Components/Common/Rating.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Star from '../../Assets/Icons/star.svg';
 
-function Rating(): JSX.Element {
+interface Props {
+  rating: number;
+}
+function Rating({ rating }: Props): JSX.Element {
   return (
     <figure className="rating">
       <img src={Star} alt="podcastLogo" />
-      <figcaption>5.0</figcaption>
+      <figcaption>{rating !== 0 ? rating : 'Not rated'}</figcaption>
     </figure>
   );
 }
