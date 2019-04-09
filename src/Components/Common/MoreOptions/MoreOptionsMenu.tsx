722e09d04a7076ee231fb9ac8e5e15189f69bf50
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-09 10:42:52 +0200

    Add func to enable toggeling RateEpisodes modal
    Issue #25

--- src/Components/Common/MoreOptions/MoreOptionsMenu.tsx
+++ src/Components/Common/MoreOptions/MoreOptionsMenu.tsx
@@ -4,14 +4,15 @@ import { Episode } from '../../../Models/Episode';
 import Config from '../../../Config/config';
 
 interface Props {
-  setAndToggleMoreOptionsModal: () => void;
+  toggleMoreOptionsModal: () => void;
   toggleRecommendToUserModal: () => void;
+  toggleRateEpisodeModal: () => void;
   episode?: Episode;
   podcast?: Podcast;
 }
 
 function MoreOptionsMenu({
-  setAndToggleMoreOptionsModal, toggleRecommendToUserModal, episode, podcast,
+  toggleMoreOptionsModal, toggleRecommendToUserModal, toggleRateEpisodeModal, episode, podcast,
 }: Props): JSX.Element {
   const item = episode || podcast;
 
@@ -34,10 +35,24 @@ function MoreOptionsMenu({
         className="close-more-options-modal-button"
         type="button"
         aria-label="close-more-options-modal-button"
-        onClick={setAndToggleMoreOptionsModal}
+        onClick={toggleMoreOptionsModal}
       />
       <nav>
         <ul>
+          {item instanceof Episode
+            && (
+              <li>
+                <button
+                  className="open-recommend-modal-button"
+                  type="button"
+                  aria-label="open-recommend-modal-button"
+                  onClick={toggleRateEpisodeModal}
+                >
+                  Rate
+                </button>
+              </li>
+            )
+          }
           <li>
             <button
               className="open-recommend-modal-button"
