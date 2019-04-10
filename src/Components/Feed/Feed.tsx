4baee4ac7c56b97562c2ca34602a58345daf9397
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-04-10 13:13:35 +0200

    Add View for user feed
    Issue #19

---
+++ src/Components/Feed/Feed.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Feed = (): JSX.Element => (
+  <div>
+    <h3>Feed</h3>
+  </div>
+);
+
+export default Feed;
---
+++ src/Views/Feed.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FeedComponent from '../Components/Feed/Feed';
+
+const Flow = (): JSX.Element => (
+  <div className="Flow">
+    <FeedComponent />
+  </div>
+);
+
+export default Flow;
