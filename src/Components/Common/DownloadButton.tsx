e91a4dd2f3ca542a6104ae78fe054f55a78a0429
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2019-05-11 18:50:32 +0200

    Add func to stop playback when "downloading" file on ios

--- src/Components/Common/DownloadButton.tsx
+++ src/Components/Common/DownloadButton.tsx
@@ -7,15 +7,19 @@ import { Episode } from '../../Models/Episode';
 interface Props {
   episode: Episode;
   download: (episode: Episode) => void;
+  stop: () => void;
   isDownloading: string;
 }
-function DownloadButton({ episode, download, isDownloading }: Props): JSX.Element {
+function DownloadButton({
+  episode, download, isDownloading, stop,
+}: Props): JSX.Element {
   const downloadEpisode = (): void => {
     if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
       const win = window.open(typeof episode.audio === 'string' ? episode.audio : '', '_blank');
       if (win) {
         win.focus();
       }
+      stop();
     } else {
       download(episode);
     }
